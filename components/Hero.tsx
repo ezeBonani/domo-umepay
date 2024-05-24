9c6ad7cc0da85b4046b352e73ccb49d68fef89c0
Ezequiel Luis Bonani <eze.bonani@gmail.com>
2024-05-24 14:16:55 -0300

    suspense text loading

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <section className="flex flex-col gap-20 md:gap-28 xl:flex-row font-serif">
       <div className="heroVideo relative">
-        <Suspense fallback={<p className='w-full'>Loading video....</p>}>
+        <Suspense fallback={<p className='w-full text-slate-800'>Loading video....</p>}>
           <video autoPlay loop muted src="/drone.webm" className='w-full'></video>
         </Suspense>
         <h1 className='absolute top-1/2 left-4 sm:left-10 text-white text-lg sm:text-4xl lg:text-5xl w-3/4 border-l-2 pl-4 [text-shadow:_0_2px_0_rgb(0_0_0_/_30%)] animate-fade' >Conocé un lugar único de las sierras de Córdoba</h1>
