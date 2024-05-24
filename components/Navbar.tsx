02a6adfeeb40d24b9616b7f5502de09f8880ffd6
Ezequiel Luis Bonani <eze.bonani@gmail.com>
2024-05-23 23:39:46 -0300

    resize hamb menu button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,7 +32,7 @@ function Navbar() {
       </ul>
 
       {/* Responsibe Menu */}
-      <Image src="/menu.png" alt="menu" width={32} height={32} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className="inline-block cursor-pointer lg:hidden z-50 bg-white rounded-lg" onClick={handleMenu} />
+      <Image src="/menu.png" alt="menu" width={29} height={29} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className="inline-block cursor-pointer lg:hidden z-50 bg-white rounded-lg" onClick={handleMenu} />
       
       <ul className={`absolute top-0 items-center justify-evenly z-40 duration-500 transition-all bg-slate-700 opacity-90 text-white h-[100vh] ${isOpen ? "w-[100vw] right-0 py-8 lg:w-0 lg:right-[-50px] flex flex-col " : "w-0 right-[-50px]"}`}>
         {NAV_LINKS.map((link)=> (<Link href={link.href} key={link.key} className={`regular-16 text-grey-50  cursor-pointer pd-1.5 transition-all duration-500 hover:font-bold ${isOpen ? "flexCenter" : "hidden"}`} onClick={()=>setIsOpen(false)} >{link.label} </Link>))}
