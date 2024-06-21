69c71549586f25845ed50daae6c248c18aace31d
Ezequiel Luis Bonani <eze.bonani@gmail.com>
2024-06-20 23:49:50 -0300

    adjust reviews imgs

--- components/Domo.tsx
+++ components/Domo.tsx
@@ -10,9 +10,9 @@ function Domo() {
           <h2 className='text-4xl md:text-6xl mt-6 tracking-wider'>Domo Umepay</h2>
           <h3 className='mb-6'>- Glamping de Montaña -</h3>
           <p className='text-base md:text-lg text-center py-4 md:w-3/4' >Disfrutá de un entorno natural con tecnología y confort en un domo geodésico ubicado en el barrio &quot;La Aldea&quot; dentro de la Ecoaldea Umepay, a 30 minutos de Villa Yacanto de Calamuchita</p>
-          <div className="mt-2 md:mt-10 text-center flex justify-center items-center gap-8 md:gap-20 text-xs md:text-sm text-wrap">
-            <Image src={reviewsImg[0].src} width={200} height={100} alt="superhost"/>
-            <Image src={reviewsImg[1].src} width={200} height={100} alt="google5stars"/>
+          <div className="mt-6 md:mt-10 text-center flex flex-col md:flex-row justify-center items-center gap-6 md:gap-20 text-xs md:text-sm text-wrap">
+            <Image src={reviewsImg[0].src} width={170} height={100} alt="superhost"/>
+            <Image src={reviewsImg[1].src} width={170} height={100} alt="google5stars"/>
           </div>
         </div>
       </div>
