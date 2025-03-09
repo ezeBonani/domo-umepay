b66ed3e48ab2a28348084726947d90da32664350
Ezequiel Luis Bonani <eze.bonani@gmail.com>
2025-03-09 18:01:53 -0300

    add underline when hover in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,7 +28,7 @@ function Navbar() {
       </Link>
 
       <ul className="hidden h-full lg:flex justify-between flex-1">
-        {NAV_LINKS.map((link)=> (<Link href={link.href} key={link.key} className="regular-16 text-grey-50 flexCenter cursor-pointer pd-1.5"><span className="transition-all hover:scale-110 duration-300">{link.label}</span></Link>))}
+        {NAV_LINKS.map((link)=> (<Link href={link.href} key={link.key} className="regular-16 text-grey-50 flexCenter cursor-pointer pd-1.5"><span className="relative text-black hover:text-gray-500 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-gray-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-gray-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">{link.label}</span></Link>))}
       </ul>
 
       {/* Responsibe Menu */}
